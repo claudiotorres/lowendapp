d3a175091d94c5ac65a17731c29077a6bb6a8b8d
claudiotorres <claudio@claudiotorres.com.br>
2012-11-24 22:22:57 +0100

    FIrst Rails
    My first rail app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0871411c0f2550ea16d18dd75c3dff43463d80c65f352f5de2d73138f20fc44c3d0ea28e840070186a10c83367f477d46703ee1a35d5f6d54a3d179222501823'
